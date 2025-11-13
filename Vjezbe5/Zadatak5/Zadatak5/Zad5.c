552e463a07d74bbb2d0290c76c46b3138dec77c3
fila299 <120975794+fila299@users.noreply.github.com>
2025-11-13 03:06:58 +0100

    Update Zad5.c

--- Vjezbe5/Zadatak5/Zadatak5/Zad5.c
+++ Vjezbe5/Zadatak5/Zadatak5/Zad5.c
@@ -1,195 +1,89 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS_
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-#define MAX_LINE 1024
-#define ALLOCATION_ERROR -1
-#define FILE_NOT_FOUND_ERROR -2
-#define ERROR -3
+typedef struct Node {
+    double value;
+    struct Node* next;
+} Node;
 
-struct _member;
-typedef struct _member* position;
-typedef struct _member {
-	float number;
-	position next;
-}member;
+// dodaj na stog
+Node* push(Node* top, double value) {
+    Node* n = (Node*)malloc(sizeof(Node));
+    n->value = value;
+    n->next = top;
+    return n;
+}
 
-float fileinput(char filename[MAX_LINE]);
-int printPostfix(char filename[MAX_LINE]);
-int pop(float* temp);
-int push(float* temp);
-int Delete();
+// skini sa stoga
+Node* pop(Node* top, double* value) {
+    if (top == NULL) {
+        printf("Greska: Neispravan postfiksni izraz (premalo operanada)!\n");
+        exit(1);
+    *value = top->value;
+    Node* temp = top;
+    top = top->next;
+    free(temp);
+    return top;
+}
 
-position top = NULL;
+// primijeni operator
+    double op(double a, double b, char c) {
+        if (c == '+') return a + b;
+        if (c == '-') return a - b;
+        if (c == '*') return a * b;
+        if (c == '/') {
+            //provjera dijeljenja s nulom
+            if (b == 0) {
+                printf("Greska: Dijeljenje s nulom!\n");
+                exit(1);
+            }
+            return a / b;
+        }
+
+        // ako je nepoznat operator
+        printf("Greska: Nepoznat operator '%c'!\n", c);
+        exit(1);
+    }
+
+// izračunaj postfiks izraz
+double eval(char* exp) {
+    Node* s = NULL;
+    char t[50];
+    int i = 0;
+
+    while (sscanf(exp + i, "%s", t) == 1) {
+        i += strlen(t) + 1;
+        if (isdigit(t[0]) || (t[0] == '-' && isdigit(t[1]))) {
+            s = push(s, atof(t));
+        }
+        else {
+            double b, a;
+            s = pop(s, &b);
+            s = pop(s, &a);
+            s = push(s, op(a, b, t[0]));
+        }
+    }
+
+    double r;
+    s = pop(s, &r);
+    return r;
+}
 
 int main() {
-	float result;
+    FILE* f = fopen("Stack.txt", "r");
+    if (!f) {
+        printf("File error!\n");
+        return 1;
+    }
 
-	char filename[MAX_LINE] = "Stack.txt";
+    char exp[256];
+    fgets(exp, sizeof(exp), f);
+    fclose(f);
 
-	printf("Citam postfiks izraz iz datoteke: %s\n", filename);
-
-	printPostfix(filename);
-
-	result = fileinput(filename);
-	if (result != ERROR) {
-		printf("\nRezultat je %.2f.", result);
-	}
-	else {
-		printf("Netocno postavljena jednadzba u datoteci!");
-	}
-
-	Delete();
-	return 0;
-}
-
-float fileinput(char filename[MAX_LINE]) {
-	FILE* fp = NULL;
-	fp = fopen(filename, "r");
-	if (fp == NULL) {
-		printf("Datoteka '%s' nije pronadjena!\n", filename);
-		return FILE_NOT_FOUND_ERROR;
-	}
-	int n = 0;
-	int l;
-	float temp1 = 0;
-	float temp2 = 0;
-	float* tmp1 = &temp1;
-	float* tmp2 = &temp2;
-	char ctemp[MAX_LINE];
-	char oper[MAX_LINE];
-	float res;
-	float* tempres;
-	tempres = &res;
-
-	fgets(ctemp, MAX_LINE, fp);
-	fclose(fp);
-
-	char* p = ctemp;
-
-	while (strlen(p) > 0 && p[0] != '\n') {
-		l = sscanf(p, "%f %n", &temp1, &n);
-
-		if (l == 1) {
-			push(tmp1);
-		}
-		else {
-			l = sscanf(p, "%s %n", oper, &n);
-			if (l != 1) break;
-
-			if (!strcmp(oper, "+")) {
-				pop(tmp1);
-				pop(tmp2);
-				if (temp1 == ERROR || temp2 == ERROR) return ERROR;
-				res = temp2 + temp1;
-				push(tempres);
-			}
-			else if (!strcmp(oper, "-")) {
-				pop(tmp1);
-				pop(tmp2);
-				if (temp1 == ERROR || temp2 == ERROR) return ERROR;
-				res = temp2 - temp1;
-				push(tempres);
-			}
-			else if (!strcmp(oper, "*")) {
-				pop(tmp1);
-				pop(tmp2);
-				if (temp1 == ERROR || temp2 == ERROR) return ERROR;
-				res = temp2 * temp1;
-				push(tempres);
-			}
-			else if (!strcmp(oper, "/")) {
-				pop(tmp1);
-				if (temp1 == 0) {
-					printf("Dijeljenje s 0 nije moguce!\n");
-					return ERROR;
-				}
-				pop(tmp2);
-				if (temp1 == ERROR || temp2 == ERROR) return ERROR;
-				res = temp2 / temp1;
-				push(tempres);
-			}
-			else {
-				if (n == 0) n = 1;
-			}
-
-		}
-		p += n;
-	}
-
-	pop(tmp1);
-
-	if (top != NULL) {
-		printf("Greska: Vise elemenata na stogu, krivi izraz.\n");
-		return ERROR;
-	}
-
-	return *tmp1;
-}
-
-int printPostfix(char filename[MAX_LINE])
-{
-	printf("\nPostfiks iz datoteke: ");
-
-	FILE* fp = NULL;
-	fp = fopen(filename, "r");
-	if (fp == NULL) {
-		printf("Datoteka '%s' jos ne postoji.\n", filename);
-		return FILE_NOT_FOUND_ERROR;
-	}
-
-	char line[MAX_LINE];
-
-	while (fgets(line, MAX_LINE, fp) != NULL)
-	{
-		printf("%s", line);
-	}
-
-	fclose(fp);
-	printf("\n");
-	return 0;
-}
-
-int push(float* temp) {
-	position q;
-	q = (position)malloc(sizeof(member));
-	if (q == NULL) {
-		printf("Ne mogu alocirati memoriju!\n");
-		return ALLOCATION_ERROR;
-	}
-	q->number = *temp;
-	q->next = top;
-	top = q;
-	return 0;
-}
-
-int pop(float* temp) {
-	position q;
-	if (top == NULL) {
-		printf("Stog je prazan!\n");
-		*temp = ERROR;
-		return ERROR;
-	}
-
-	*temp = top->number;
-
-	q = top;
-	top = top->next;
-	free(q);
-	return 0;
-}
-
-int Delete() {
-	position head = top;
-	position temp;
-
-	while (head != NULL) {
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
-	top = NULL;
-	return 0;
+    printf("Result = %.2lf\n", eval(exp));
+    return 0;
 }
